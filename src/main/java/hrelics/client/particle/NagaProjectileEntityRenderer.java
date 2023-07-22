e1fa4948f7376351a2850c7f6c3046cac6d0458a
Sadfahlsdj <124646333+Sadfahlsdj@users.noreply.github.com>
2023-07-21 20:40:44 -0700

    naga projectile spriet change

--- src/main/java/hrelics/client/particle/NagaProjectileEntityRenderer.java
+++ src/main/java/hrelics/client/particle/NagaProjectileEntityRenderer.java
@@ -30,12 +30,8 @@ public class NagaProjectileEntityRenderer extends EntityRenderer<NagaProjectileE
     @Override
     public void render(NagaProjectileEntity entity, float yaw, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light){
         matrices.push();
+        //rotates da projectile
         matrices.multiply(RotationAxis.POSITIVE_Y.rotation(entity.age));
-
-        //logic to rotate the projectile goes here probably - commented shit is testing
-        //matrices.multiply(Direction.fromHorizontal(0).getRotationQuaternion());
-        //Quaternionf q = new Quaternionf(entity.age % 20, 0, 0, 0);
-        //matrices.multiply(q);
 
         MinecraftClient.getInstance().getItemRenderer().renderItem(
                 STACK, ModelTransformation.Mode.FIXED, light, OverlayTexture.DEFAULT_UV, matrices, vertexConsumers, 0
